f791d07b818eb0291bcc948f513091094679369f
Michal <szpunar98@gmail.com>
2020-06-05 17:11:21 +0200

    started to work on an admin page

--- db/queries.py
+++ db/queries.py
@@ -1,3 +1,5 @@
 class Queries:
     query_login_page = "SELECT id_stanowisko FROM Pracownicy WHERE id_pracownika = :id AND haslo = :password"
+    query_get_tables = "SELECT table_name FROM user_tables ORDER BY table_name"
+    query_get_list = "SELECT {cols} FROM {table} ORDER BY {param}"
 
